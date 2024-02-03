3d3cca3004f9d363a4b5fc8460c7320a8d2dc672
IbnuJabir <kedirjabir12@gmail.com>
2024-02-03 10:37:31 -0800

    handle empty stack

--- b.m
+++ b.m
@@ -1,3 +1,1 @@
-push 98
-pop Holberton
-pall
+pop
--- pop.c
+++ pop.c
@@ -10,7 +10,7 @@
 void pop(stack_t **stack, unsigned int line_number)
 {
     stack_t *temp;
-
+	arg = NULL;
     if (*stack == NULL)
     {
         fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
